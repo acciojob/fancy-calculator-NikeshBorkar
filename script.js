d590f7c4d9ffcfa7c04dbeda60c4f8868551a254
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-07-03 22:37:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function isSameType(value1, value2) {
+  //your js code here
+	let x=value1*2+value2
+	return `The final result is: ${x}`
+}
+
+// do not change the code below.
+let value1 = prompt("Enter Start of the Range.");
+let value2 = prompt("Enter End Of the Range.");
+alert(isSameType(value1, value2));
+
